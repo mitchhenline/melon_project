2293cf10eb45c14a8f68dc4155974fc8c08ec1d5
Mitch Henline <rmhenline@gmail.com>
2022-12-05 13:27:20 -0700

    first commit

---
+++ melons.py
@@ -0,0 +1,46 @@
+import csv
+from pprint import pprint
+
+class Melon():
+    def __init__(self, melon_id, common_name, price, image_url, color, seedless):
+        self.melon_id = melon_id
+        self.price = price
+        self.common_name = common_name
+        self.image_url = image_url
+        self.color = color
+        self.seedless = seedless
+
+    def __repr__(self):
+        return f"<melon: {self.melon_id}, {self.common_name}>"
+
+    def price_str(self):
+        return f"${self.price:.2f}"
+
+melon_dict = {}
+
+with open("melons.csv") as csvfile:
+    rows = csv.DictReader(csvfile)
+
+    for row in rows:
+        melon_id = row['melon_id']
+        melon = Melon(
+            melon_id,
+            row['common_name'],
+            float(row['price']),
+            row['image_url'],
+            row['color'],
+            eval(row['seedless']))
+
+        melon_dict[melon_id] = melon
+        pprint(melon_dict)
+        
+
+
+def read_melon(file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            pprint(row)
+
+# read_melon("melons.csv")
