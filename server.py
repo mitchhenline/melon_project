28b4e547d8647c4b09ff879cce8be9129c2e67e0
Mitch Henline <rmhenline@gmail.com>
2022-12-06 15:18:08 -0700

    part 2 almost complete

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-import jinja2
+import jinja2, melons
 
 app = Flask(__name__)
 app.jinja_env.undefined = jinja2.StrictUndefined
@@ -12,12 +12,15 @@ def homepage():
 
 
 @app.route('/melons')
-def melons():
-    return render_template("melons.html")
+def all_melons():
+    melon_list = melons.get_melon_list()
+    return render_template("all_melons.html", melon_list = melon_list)
 
 @app.route('/melon/<melon_id>')
-def melon_id(melon_id):
-    return render_template("melon.html")
+def melon_details(melon_id):
+
+    melon = melons.get_melon_id(melon_id)
+    return render_template("melon_details.html", melon=melon)
 
 @app.route('/add_to_cart/<melon_id>')
 def add_to_cart(melon_id):
@@ -25,7 +28,7 @@ def add_to_cart(melon_id):
 
 @app.route('/cart')
 def show_cart():
-    return render_template('cart.html')
+    return render_template('show_shopping_cart.html')
 
 
 # ______ ROUTES END _______
