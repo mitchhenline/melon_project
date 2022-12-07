a60a48b95b788ca1784853fecf3f21078e092711
Mitch Henline <rmhenline@gmail.com>
2022-12-07 12:32:32 -0700

    finished shopping cart functionality

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, flash, request, session
 import jinja2, melons
 
 app = Flask(__name__)
 app.jinja_env.undefined = jinja2.StrictUndefined
+app.secret_key = 'dev'
 
 # ______ ROUTES _______
 
@@ -24,11 +25,43 @@ def melon_details(melon_id):
 
 @app.route('/add_to_cart/<melon_id>')
 def add_to_cart(melon_id):
-    return f"{melon_id} added to cart"
+    
+    if 'cart' not in session:
+        session['cart'] = {}
+    cart = session['cart']
+
+    cart[melon_id] = cart.get(melon_id, 0) + 1
+    session.modified = True
+    flash(f"Melon {melon_id} successfully added to cart.")
+    print(cart)
+
+    return redirect("/cart")
 
 @app.route('/cart')
 def show_cart():
-    return render_template('show_shopping_cart.html')
+    order_total = 0
+    cart_melons = []
+
+    cart = session.get("cart", {})
+
+    for melon_id, quantity in cart.items():
+        melon = melons.get_melon_id(melon_id)
+
+        total_cost = quantity * melon.price
+        order_total +=total_cost
+
+        melon.quantity = quantity
+        melon.total_cost = total_cost
+
+        cart_melons.append(melon)
+
+    return render_template('cart.html', cart_melons=cart_melons, order_total=order_total)
+
+@app.route('/empty_cart')
+def empty_cart():
+    session['cart'] = {}
+
+    return redirect('/cart')
 
 
 # ______ ROUTES END _______
